1b50ade5550cdf1a5d3817cdb35e2702f4c75979
jethoo <jeewanbhusal@gmail.com>
2021-03-03 23:12:15 -0500

    Custom Player D_11 play/pause/fastforward functionality added

---
+++ D_11_CustomVideoPlayer/scripts.js
@@ -0,0 +1,37 @@
+/* Get our elements */
+const player = document.querySelector('.player')
+const video = player.querySelector('.viewer')
+const progress = player.querySelector('.progress')
+const progressBar = player.querySelector('.progress-filled')
+const toggle = player.querySelector('.toggle')
+const skipButtons = player.querySelectorAll('[data-skip]')
+const ranges = player.querySelectorAll('.player__slider')
+
+/* Build out functions */
+function togglePlay(){
+    const method = video.paused ? 'play' : 'pause'
+    video[method]()
+}
+
+function updateButton(){
+    const icon = this.paused ? '►' : '❚ ❚'
+    toggle.textContent = icon
+}
+
+function skip(){
+    //console.log('Skipping', Skipping)
+    video.currentTime += parseFloat(this.dataset.skip)
+}
+
+function handleRangeUpdate(){
+
+}
+
+/* Hook up the event listener */
+video.addEventListener('click', togglePlay)
+video.addEventListener('play', updateButton)
+video.addEventListener('pause', updateButton)
+
+toggle.addEventListener('click', togglePlay)
+skipButtons.forEach(button => button.addEventListener('click', skip))
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
